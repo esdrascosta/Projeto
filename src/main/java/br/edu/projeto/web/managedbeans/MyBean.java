b85787bbe7d121b4ae056cb9e2256348595e6f79
esdras <servoesdras@gmail.com>
2014-01-12 16:55:55 -0300

    first commit

---
+++ src/main/java/br/edu/projeto/web/managedbeans/MyBean.java
@@ -0,0 +1,25 @@
+package br.edu.projeto.web.managedbeans;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+@ManagedBean
+public class MyBean {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void destroyWorld(ActionEvent actionEvent) {
+		FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO,
+				"Erro no Sistema", "Porfavor Tente mais tarde");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+	}
+}
